09b24472be291752ffb8e731ae6b65bbb4c61ccc
rkcho317 <rkcho317@csu.fullerton.edu>
2023-05-05 14:47:42 -0700

    added movie reducer and mapper classes

--- movierec/src/main/java/Main.java
+++ movierec/src/main/java/Main.java
@@ -79,6 +79,60 @@ public class Main {
         }
     }
 
+    public static class MovieMapper extends Mapper<Object, Text, Text, Text> {
+
+        private Text outKey = new Text();
+        private Text outValue = new Text();
+
+        public void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+            String[] tokens = value.toString().split(",");
+            String userId = tokens[0];
+            String movieId = tokens[1];
+            String rating = tokens[2];
+            outKey.set(userId);
+            outValue.set(movieId + ":" + rating);
+            context.write(outKey, outValue);
+        }
+    }
+
+    public static class MovieReducer extends Reducer<Text, Text, Text, Text> {
+
+        private Text outKey = new Text();
+        private Text outValue = new Text();
+        private Map<String, Float> movieRatings = new HashMap<>();
+
+        public void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+            for (Text value : values) {
+                String[] tokens = value.toString().split(":");
+                String movieId = tokens[0];
+                float rating = Float.parseFloat(tokens[1]);
+                if (movieRatings.containsKey(movieId)) {
+                    rating = (rating + movieRatings.get(movieId)) / 2.0f;
+                }
+                movieRatings.put(movieId, rating);
+            }
+
+            List<Map.Entry<String, Float>> list = new ArrayList<>(movieRatings.entrySet());
+            list.sort(Map.Entry.comparingByValue());
+
+            String recommendedMovies = "";
+            int count = 0;
+            for (Map.Entry<String, Float> entry : list) {
+                recommendedMovies += entry.getKey() + ",";
+                count++;
+                if (count >= 5) {
+                    break;
+                }
+            }
+            outKey.set(key);
+            outValue.set(recommendedMovies);
+            context.write(outKey, outValue);
+            movieRatings.clear();
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         SparkConf conf = new SparkConf().setAppName("movie_recommendation").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
@@ -134,5 +188,16 @@ public class Main {
         title_principals_df.show();
         title_ratings_df.show();
 
+        job.setJarByClass(MovieRecommender.class);
+        job.setMapperClass(MovieMapper.class);
+        job.setReducerClass(MovieReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
     }
